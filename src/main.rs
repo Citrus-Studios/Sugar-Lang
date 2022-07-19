c7972cfe1ccd04d7a61e9ca1271020848c992555
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-19 16:55:35 -0400

    Added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
