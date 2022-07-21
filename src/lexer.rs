0eb3b8db6e487133158092538663144d5c88b674
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-21 18:31:26 -0400

    Added Input to the struct and added run function

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,11 +1,18 @@
 pub struct Lexer {
-
+    input: String
 }
 
 impl Lexer {
-    pub fn new() -> Self {
+    pub fn new(input: String) -> Self {
         Self {
-
+            input
         }
+    }
+    pub fn run(&mut self) -> () {
+        
+        for x in self.input.chars() {
+            
+        }
+        todo!();
     }
 }
