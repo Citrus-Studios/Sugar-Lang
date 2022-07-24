df4b8d6bd17cf3c3410aaa01fabde3e2edc33fe8
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-24 16:50:21 -0400

    Added Line and Char pos, and added converting to a tok_struct

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,15 +2,27 @@ use crate::tokens::{Tokens, TokensStruct};
 
 pub struct Lexer {
     input: String,
+    line: u128,
+    char_pos: u128,
 }
 
 impl Lexer {
     pub fn new(input: String) -> Self {
-        Self { input }
+        Self {
+            input,
+            line: 1,
+            char_pos: 1,
+        }
     }
     pub fn run(&mut self) -> Vec<TokensStruct> {
         for x in self.input.chars() {
             let tok: Tokens = x.to_string().into();
+            let tok_struct = TokensStruct {
+                token: tok,
+                char: x,
+                line: self.line,
+                char_pos: self.char_pos,
+            };
         }
         todo!();
     }
