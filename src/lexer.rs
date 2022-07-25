027760681920463677318f84b18ffdab279447e1
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-25 16:20:51 -0400

    Implemented second_pass

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use crate::tokens::{Tokens, TokensStruct};
+use crate::tokens::{CollectCharStrings, SameVecType, Tokens, TokensStruct};
 
 pub struct Lexer {
     input: String,
@@ -41,11 +41,36 @@ impl Lexer {
     /// Runs a second pass of the Lexer
     pub fn second_pass(&mut self) {
         let tokens_clone = self.tokens.clone();
-        for x in tokens_clone {}
+        self.tokens.clear();
+        let mut ident_mode = false;
+        let mut ident_cache = String::new();
+        for (_i, x) in tokens_clone.iter().enumerate() {
+            match x.token {
+                Tokens::Ident => {
+                    ident_mode = true;
+                    ident_cache.push_str(x.string.as_str());
+                }
+                _ => {
+                    if ident_mode {
+                        self.tokens.push(TokensStruct {
+                            token: x.clone().token,
+                            string: ident_cache,
+                            char_pos: x.char_pos,
+                            line: x.line,
+                        });
+                        ident_cache = String::new();
+                        ident_mode = false;
+                    } else if x.token != Tokens::Space {
+                        self.tokens.push(x.clone());
+                    }
+                }
+            }
+        }
     }
     /// Runs the Lexer
     pub fn run(&mut self) -> Vec<TokensStruct> {
         self.first_pass();
-        todo!();
+        self.second_pass();
+        self.tokens.clone()
     }
 }
