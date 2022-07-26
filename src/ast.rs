22f185f3aae68053a4b949781756ed4509604449
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-25 21:40:26 -0400

    Implemented more Syntax Types and a ASTStruct

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,28 @@
 pub enum AST<T> {
     Block { scope: usize, contents: Vec<AST<T>> },
     Literal(T),
+    Return,
+    Define,
+    Declare,
+    Type(String),
+    Symbol(Symbol),
+}
+
+pub struct ASTStruct<T> {
+    ast: AST<T>,
+    char_pos: u128,
+    line: u128,
+}
+
+pub enum Symbol {
+    Equal,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Equality,
+    Bang,
+    Arrow,
+    SemiColon,
+    Colon,
 }
