2068fb0c3b2079cd528eb2f20aa99f3099794c52
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-26 22:09:19 -0400

    did a lot of shit don't feel like explaining

--- src/parser.rs
+++ src/parser.rs
@@ -16,7 +16,7 @@ impl Parser {
             tokens,
             syntax_tree: ASTStruct {
                 ast: AST::Block {
-                    scope: 0,
+                    scope: 1,
                     contents: vec![],
                 },
                 char_pos: 0,
@@ -26,15 +26,40 @@ impl Parser {
     }
     pub fn run(mut self) -> ASTStruct {
         let mut last_token: Option<TokensStruct> = None;
-        let block = ASTStruct::get_block(&mut self.syntax_tree).unwrap();
+        let mut block = ASTStruct::get_block(&mut self.syntax_tree).unwrap();
         for x in self.tokens.clone() {
+            let mut matched = false;
             // info!("CurTokens: {:#?} {:#?}", x, last_token);
             match last_token {
                 Some(v) => {
                     let y = v.token.clone();
                     let z = x.token.clone();
-                    info!("{y:#?}, {z:#?}");
-                    info!("{}, {}", v.string, x.string);
+                    info!("{y:#?}, {z:#?} | {}, {}", v.string, x.string);
+
+                    // new block
+                    if y == Tokens::Delimiter && v.string.as_str() == "{" {
+                        block.1.push(ASTStruct {
+                            ast: AST::Block {
+                                scope: *block.0 + 1,
+                                contents: vec![],
+                            },
+                            line: v.line,
+                            char_pos: v.char_pos,
+                        });
+                        let len = block.1.clone().len();
+                        block = ASTStruct::get_block(&mut block.1[len - 1]).unwrap();
+                    }
+                    // go up a block
+                    if y == Tokens::Delimiter && v.string.as_str() == "}" {
+                        info!("{}, {}", block.0, block.0.clone() - 1);
+                        block = ASTStruct::get_block(&mut self.syntax_tree).unwrap();
+                        for i in 0..(*block.0 - 1) {
+                            info!("Iteration {i}");
+                            let len = block.1.clone().len();
+                            block = ASTStruct::get_block(&mut block.1[len - 1]).unwrap();
+                        }
+                    }
+
                     // ->
                     if y == Tokens::Subtract && z == Tokens::Greater {
                         info!("Arrow ran!");
@@ -43,15 +68,7 @@ impl Parser {
                             line: x.line,
                             char_pos: x.char_pos,
                         });
-                    }
-                    // =
-                    if y == Tokens::Equal && z != Tokens::Equal {
-                        info!("Equal ran!");
-                        block.1.push(ASTStruct {
-                            ast: AST::Symbol(Symbol::Equal),
-                            line: x.line,
-                            char_pos: x.char_pos,
-                        });
+                        matched = true;
                     }
                     // ==
                     if y == Tokens::Equal && z == Tokens::Equal {
@@ -61,6 +78,17 @@ impl Parser {
                             line: x.line,
                             char_pos: x.char_pos,
                         });
+                        matched = true;
+                    }
+                    // =
+                    if y == Tokens::Equal && z != Tokens::Equal && !matched {
+                        info!("Equal ran!");
+                        block.1.push(ASTStruct {
+                            ast: AST::Symbol(Symbol::Equal),
+                            line: x.line,
+                            char_pos: x.char_pos,
+                        });
+                        matched = true;
                     }
                     // !
                     if y == Tokens::Bang && z != Tokens::Equal {
@@ -70,6 +98,7 @@ impl Parser {
                             line: x.line,
                             char_pos: x.char_pos,
                         });
+                        matched = true;
                     }
                     // !=
                     if y == Tokens::Bang && z == Tokens::Equal {
@@ -79,14 +108,56 @@ impl Parser {
                             line: x.line,
                             char_pos: x.char_pos,
                         });
+                        matched = true;
                     }
                     // dec
                     if y == Tokens::Ident && v.string.as_str() == "dec" {
                         info!("Declare ran!");
                         block.1.push(ASTStruct {
                             ast: AST::Declare,
-                            line: x.line,
-                            char_pos: x.char_pos,
+                            line: v.line,
+                            char_pos: v.char_pos,
+                        });
+                        matched = true;
+                    }
+                    // def
+                    if y == Tokens::Ident && v.string.as_str() == "def" {
+                        info!("Declare ran!");
+                        block.1.push(ASTStruct {
+                            ast: AST::Define,
+                            line: v.line,
+                            char_pos: v.char_pos,
+                        });
+                        matched = true;
+                    }
+                    // byte
+                    if y == Tokens::Ident && v.string.as_str() == "byte" {
+                        info!("Matched Byte");
+                        block.1.push(ASTStruct {
+                            ast: AST::Type("byte".to_string()),
+                            line: v.line,
+                            char_pos: v.char_pos,
+                        });
+                        matched = true;
+                    }
+                    // ret
+                    if y == Tokens::Ident && v.string.as_str() == "ret" {
+                        info!("Return Matched");
+                        block.1.push(ASTStruct {
+                            ast: AST::Return,
+                            line: v.line,
+                            char_pos: v.char_pos,
+                        });
+                        matched = true;
+                    }
+
+                    // Misc Names
+                    if !matched && y == Tokens::Ident {
+                        info!("Name Matched");
+                        block.1.push(ASTStruct {
+                            ast: AST::Name(v.string),
+                            line: v.line,
+                            char_pos: v.char_pos,
                         });
                     }
                 }
