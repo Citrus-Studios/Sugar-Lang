cf561b16dfeb304f6d38047121a4b6c7915385e5
lonnonjamesd <lemonjameslem@protonmail.com>
2022-09-07 17:35:27 -0400

    Added `Context`

--- src/llvm_compiler/compiler.rs
+++ src/llvm_compiler/compiler.rs
@@ -1,12 +1,18 @@
+use inkwell::context::Context;
+
 use crate::parser::{Expr, Expr_};
 
 pub struct Compiler {
     ast: Vec<Expr>,
+    context: Context,
 }
 
 impl Compiler {
     pub fn new(ast: Vec<Expr>) -> Self {
-        Self { ast }
+        Self {
+            ast,
+            context: Context::create(),
+        }
     }
 
     pub fn compile_llvm(self) {
