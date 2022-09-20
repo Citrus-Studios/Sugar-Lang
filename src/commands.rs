3c05753bef768e7455c771be46210aae0ae616c9
LemonjamesD <lemonjameslem@protonmail.com>
2022-09-20 02:22:58 -0400

    moved commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,1 +1,56 @@
-pub fn add_commands() {}
+use std::process::Command;
+
+use crate::Args;
+
+pub fn add_commands(args: Args) {
+	let prefix;
+	let cmd;
+
+	if cfg!(target_os = "windows") {
+		prefix = "/C";
+		cmd = "cmd";
+	} else {
+		prefix = "-c";
+		cmd = "sh";
+	}
+	Command::new(cmd)
+		.args([prefix, "llvm-dis out.bc"])
+		.output()
+		.unwrap();
+
+	Command::new(cmd)
+		.args([prefix, "llc -filetype=obj out.ll -o out.o"])
+		.output()
+		.unwrap();
+	if args.r#static {
+		if args.release {
+			Command::new(cmd)
+				.args([prefix, "ld.lld -O3 -static out.o -o out;"])
+				.output()
+				.unwrap();
+		} else {
+			Command::new(cmd)
+				.args([prefix, "ld.lld -static out.o -o out;"])
+				.output()
+				.unwrap();
+		}
+	} else {
+		if args.release {
+			Command::new(cmd)
+				.args([prefix, "ld.lld -O3 out.o -o out;"])
+				.output()
+				.unwrap();
+		} else {
+			Command::new(cmd)
+				.args([prefix, "ld.lld out.o -o out;"])
+				.output()
+				.unwrap();
+		}
+	}
+	if args.strip {
+		Command::new(cmd)
+			.args([prefix, "strip out;"])
+			.output()
+			.unwrap();
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{fs::File, process::Command};
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
-use crate::llvm_compiler::compiler::Compiler;
+use crate::commands::add_commands;
 
 mod commands;
 mod lexer;
@@ -57,56 +57,7 @@ fn main() {
 
 	// let _ = Compiler::new(program.stmts).compile_llvm();
 
-	let prefix;
-	let cmd;
-
-	if cfg!(target_os = "windows") {
-		prefix = "/C";
-		cmd = "cmd";
-	} else {
-		prefix = "-c";
-		cmd = "sh";
-	}
-	Command::new(cmd)
-		.args([prefix, "llvm-dis out.bc"])
-		.output()
-		.unwrap();
-
-	Command::new(cmd)
-		.args([prefix, "llc -filetype=obj out.ll -o out.o"])
-		.output()
-		.unwrap();
-	if args.r#static {
-		if args.release {
-			Command::new(cmd)
-				.args([prefix, "ld.lld -O3 -static out.o -o out;"])
-				.output()
-				.unwrap();
-		} else {
-			Command::new(cmd)
-				.args([prefix, "ld.lld -static out.o -o out;"])
-				.output()
-				.unwrap();
-		}
-	} else {
-		if args.release {
-			Command::new(cmd)
-				.args([prefix, "ld.lld -O3 out.o -o out;"])
-				.output()
-				.unwrap();
-		} else {
-			Command::new(cmd)
-				.args([prefix, "ld.lld out.o -o out;"])
-				.output()
-				.unwrap();
-		}
-	}
-	if args.strip {
-		Command::new(cmd)
-			.args([prefix, "strip out;"])
-			.output()
-			.unwrap();
-	}
+	add_commands(args);
 
 	let elapsed = now.elapsed();
 	info!("Elapsed: {:.3?}", elapsed);
